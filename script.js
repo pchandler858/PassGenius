e7a2f8a6cb30ba647f988ccca2b0bcd3b4a56b39
Preston C <pchandler858@gmail.com>
2023-02-10 12:43:21 -0800

    added pseudocode

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-// Assignment code here
+// Define character sets
+const lowercase = "abcdefghijklmnopqrstuvwxyz";
+const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numeric = "0123456789";
+const special = "!@#$%^&*()_+-=[]{}|;':,.<>/\"?";
 
+// Initialize password as an empty string
+let password = "";
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -10,8 +16,23 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// TODO: Generate the password
+
+// TODO: Prompt the user for password length
+
+// TODO: Prompt the user for password length
+
+// TODO: Prompt the user for the character types to include in the password
+
+// TODO: Validate that at least one character type has been selected
+
+// TODO: Create a string of all the selected character sets
+
+// TODO: Generate the password by randomly selecting characters from the selected sets
+
+// TODO: Return the generated password
